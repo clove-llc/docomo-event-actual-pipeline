f039038fa7d45abadd1aac93075ae291296aeb0c
YasuhiroTakemura <yasuhirotakemura0614@gmail.com>
2026-02-19 14:13:39 +0900

    feat: スプレッドシートの情報の取得テスト

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from google.cloud import storage
 from google.cloud import bigquery
 
+from spreadsheet import spreadsheet_test_read
 from src.bigquery_repository import BigQueryRepository
 from src.cloud_storage_repository import CloudStorageRepository
 from src.config.config import get_settings
@@ -15,6 +16,8 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
+    spreadsheet_test_read()
+
     project_id, event_actual_blob = get_settings()
 
     gcs_client = storage.Client(project=project_id)
