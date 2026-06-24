34f3d5076c7125c4d73d5d194ed9bf3bb20e4e7e
YasuhiroTakemura <yasuhirotakemura0614@gmail.com>
2026-06-24 09:57:45 +0900

    fix: 取得列を明記する

--- docomo_event_sf/models/staging/stg_facility_schedule_constraints_master.sql
+++ docomo_event_sf/models/staging/stg_facility_schedule_constraints_master.sql
@@ -1,4 +1,6 @@
 select
-    * exclude (facility_name),
-    trim(facility_name) as facility_name
+    facility_code,
+    trim(facility_name) as facility_name,
+    monthly_event_limit,
+    operating_days
 from {{ source('raw', 'RAW_FACILITY_SCHEDULE_CONSTRAINTS_MASTER') }}
--- docomo_event_sf/models/staging/stg_regional_office_schedule_constraints_master.sql
+++ docomo_event_sf/models/staging/stg_regional_office_schedule_constraints_master.sql
@@ -1,2 +1,5 @@
-select *
+select
+    regional_office_name,
+    daily_event_limit,
+    operating_days
 from {{ source('raw', 'RAW_REGIONAL_OFFICE_SCHEDULE_CONSTRAINTS_MASTER') }}
