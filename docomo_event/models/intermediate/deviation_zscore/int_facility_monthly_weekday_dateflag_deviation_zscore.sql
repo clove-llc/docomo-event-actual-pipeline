0de41fa1844ea8bde306269eaefcb872bac4ec67
YasuhiroTakemura <yasuhirotakemura0614@gmail.com>
2026-07-10 17:15:31 +0900

    query; 【BQ側】固定小数点型に変更

--- docomo_event/models/intermediate/deviation_zscore/int_facility_monthly_weekday_dateflag_deviation_zscore.sql
+++ docomo_event/models/intermediate/deviation_zscore/int_facility_monthly_weekday_dateflag_deviation_zscore.sql
@@ -4,7 +4,7 @@ SELECT
   f_d_z.month,
   f_d_z.week_number_monthly,
   f_d_z.date_flag,
-  ROUND(AVG(f_d_z.z_score), 1) AS avg_z_score
+  ROUND(AVG(CAST(f_d_z.z_score AS NUMERIC)), 1) AS avg_z_score
 FROM {{ ref("stg_facility_daily_deviation_zscore") }} AS f_d_z
 LEFT JOIN {{ ref("stg_facility_master") }} AS f
   ON f_d_z.facility_code = f.facility_code
