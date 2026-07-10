a78b830319671b266e0dc79a728c8063b1029fdc
YasuhiroTakemura <yasuhirotakemura0614@gmail.com>
2026-07-10 17:17:27 +0900

    query; 【Snow側】View作成用SQLの更新

--- docomo_event_sf/create_or_replace_view_queries/intermediate/03_benchmarks/002_int_event_decile_benchmark.sql
+++ docomo_event_sf/create_or_replace_view_queries/intermediate/03_benchmarks/002_int_event_decile_benchmark.sql
@@ -5,24 +5,24 @@
   
   
   as (
-    select
+select
     benchmark_period_key,
     benchmark_period_name,
     period_start_date,
     period_end_date,
     date_flag,
     decile_rank,
-    round(cast((percentile_cont(0.10) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p10,
-    round(cast((percentile_cont(0.20) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p20,
-    round(cast((percentile_cont(0.25) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p25,
-    round(cast((percentile_cont(0.30) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p30,
-    round(cast((percentile_cont(0.40) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p40,
-    round(cast((percentile_cont(0.50) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p50,
-    round(cast((percentile_cont(0.60) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p60,
-    round(cast((percentile_cont(0.70) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p70,
-    round(cast((percentile_cont(0.75) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p75,
-    round(cast((percentile_cont(0.90) within group (order by cast(avg_actual as float))) as number(38, 18)), 0) as p90,
-    round(cast((max(cast(avg_actual as float))) as number(38, 18)), 0) as max_performance
+    round(round(percentile_cont(0.10) within group (order by avg_actual), 2)) as p10,
+    round(round(percentile_cont(0.20) within group (order by avg_actual), 2)) as p20,
+    round(round(percentile_cont(0.25) within group (order by avg_actual), 2)) as p25,
+    round(round(percentile_cont(0.30) within group (order by avg_actual), 2)) as p30,
+    round(round(percentile_cont(0.40) within group (order by avg_actual), 2)) as p40,
+    round(round(percentile_cont(0.50) within group (order by avg_actual), 2)) as p50,
+    round(round(percentile_cont(0.60) within group (order by avg_actual), 2)) as p60,
+    round(round(percentile_cont(0.70) within group (order by avg_actual), 2)) as p70,
+    round(round(percentile_cont(0.75) within group (order by avg_actual), 2)) as p75,
+    round(round(percentile_cont(0.90) within group (order by avg_actual), 2)) as p90,
+    max(avg_actual) as max_performance
 from HARATO.INT.int_facility_event_decile_avg_actual
 group by
     benchmark_period_key,
--- docomo_event_sf/create_or_replace_view_queries/intermediate/04_deviation_zscore/002_int_facility_monthly_dateflag_deviation_zscore.sql
+++ docomo_event_sf/create_or_replace_view_queries/intermediate/04_deviation_zscore/002_int_facility_monthly_dateflag_deviation_zscore.sql
@@ -5,12 +5,12 @@
   
   
   as (
-    select distinct
+select distinct
     f.facility_code,
     f.facility_name,
     f.month,
     f.date_flag,
-    round(cast((avg(avg_z_score)) as number(38, 18)), 1) as avg_z_score
+    round(avg(avg_z_score), 1) as avg_z_score
 from HARATO.INT.int_facility_monthly_weekday_dateflag_deviation_zscore as f
 group by
     f.facility_code,
